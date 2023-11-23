4e6a7116aca94d002a5daebc523fb8c3a97ca295
PhosWave <143731182+PhosWave@users.noreply.github.com>
2023-11-23 05:04:57 -0500

    Update Project3.js

--- Project3.js
+++ Project3.js
@@ -1,120 +1,62 @@
+ document.addEventListener('DOMContentLoaded', async function () {
+    const appstate = {
+      qanswered: 0,
+      qcorrect: 0,
+      qwrong: 0,
+      currentQuestionIndex: 0,
+      questions: [],
+    };
 
-    const quizContainer = document.getElementById('quiz-container');
-    const questionElement = document.querySelector('.question');
-    const choicesElement = document.querySelector('.choices');
-    const resultElement = document.querySelector('.result');
-    const restartButton = document.getElementById('restart-btn');
+    const quizContainer = document.getElementById("quiz-container");
+    const restartButton = document.getElementById("restart-btn");
 
-    let currentQuestion = 0;
-    let score = 0;
+      const response = await fetch('https://my-json-server.typicode.com/PhosWave/Project3json/db');
+      const data = await response.json();
 
-    const questions = [
-        {
-            "question": "Which method is used to concatenate two or more strings in JavaScript?",
-            "choices": [
-                {"id": "a", "text": "concat()"},
-                {"id": "b", "text": "merge()"},
-                {"id": "c", "text": "combine()"},
-                {"id": "d", "text": "append()"}
-            ],
-            "correctAnswer": "a"
-        },
-        {
-            "question": "How do you access the third element of an array?",
-            "choices": [
-                {"id": "a", "text": "myArray[2]"},
-                {"id": "b", "text": "myArray[3]"},
-                {"id": "c", "text": "myArray.third"},
-                {"id": "d", "text": "myArray.element(3)"}
-            ],
-            "correctAnswer": "a"
-        },
-        {
-            "question": "Which array method is used to add elements to the end of an array?",
-            "choices": [
-                {"id": "a", "text": "push()"},
-                {"id": "b", "text": "pop()"},
-                {"id": "c", "text": "shift()"},
-                {"id": "d", "text": "unshift()"}
-            ],
-            "correctAnswer": "a"
-        },
-        {
-            "question": "What does the 'length' property of an array represent?",
-            "choices": [
-                {"id": "a", "text": "The number of elements in the array"},
-                {"id": "b", "text": "The maximum length of the array"},
-                {"id": "c", "text": "The average length of the array elements"},
-                {"id": "d", "text": "The length of the array type"}
-            ],
-            "correctAnswer": "a"
-        },
-        {
-            "question": "How do you remove the last element from an array?",
-            "choices": [
-                {"id": "a", "text": "myArray.removeLast()"},
-                {"id": "b", "text": "myArray.splice(-1, 1)"},
-                {"id": "c", "text": "myArray.pop()"},
-                {"id": "d", "text": "myArray.shift()"}
-            ],
-            "correctAnswer": "c"
-        }
-    ];
+      appstate.questions = data.questions;
 
-    function startQuiz() {
-        currentQuestion = 0;
-        score = 0;
-        showQuestion();
-    }
+      const quizTemplate = Handlebars.compile(document.getElementById("quiz-template").innerHTML);
 
-    function showQuestion() {
-        const currentQuiz = questions[currentQuestion];
-        questionElement.textContent = currentQuiz.question;
-        choicesElement.innerHTML = '';
 
-        currentQuiz.choices.forEach(choice => {
-            const button = document.createElement('button');
-            button.textContent = choice.text;
-            button.addEventListener('click', () => checkAnswer(choice.id));
-            choicesElement.appendChild(button);
-        });
-    }
+      quizContainer.innerHTML = quizTemplate({ questions: appstate.questions });
 
-    function checkAnswer(userChoice) {
-        const currentQuiz = questions[currentQuestion];
+      document.querySelector('.question-container').classList.add('active-question');
+   
 
-        if (userChoice === currentQuiz.correctAnswer) {
-            score++;
+
+    quizContainer.addEventListener('click', function (event) {
+      const target = event.target;
+
+
+      if (target.classList.contains('choice-btn')) {
+        const selectedAnswer = target.getAttribute('data-answer');
+        const correctAnswer = appstate.questions[appstate.currentQuestionIndex].correctAnswer;
+
+        const isIncorrect = selectedAnswer !== correctAnswer;
+
+        if (isIncorrect) {
+          target.classList.add('incorrect');
+          appstate.incorrectAnswers.push({ question: appstate.questions[appstate.currentQuestionIndex].question, selectedAnswer, correctAnswer });
         }
 
-        currentQuestion++;
-
-        if (currentQuestion < questions.length) {
-            showQuestion();
-        } else {
-            showResult();
-        }
-    }
-
-    function showResult() {
-        hideElement('#quiz');
-        showElement('#restart-btn');
-        resultElement.textContent = `Your score is ${score} out of ${questions.length}.`;
-    }
-
-    function showElement(selector) {
-        document.querySelector(selector).classList.remove('hidden');
-    }
-
-    function hideElement(selector) {
-        document.querySelector(selector).classList.add('hidden');
-    }
-
-    restartButton.addEventListener('click', () => {
-        showElement('#quiz');
-        hideElement('#restart-btn');
-        startQuiz();
+    
+        showNextQuestion();
+        appstate.qanswered = appstate.qanswered+4;
+      }
     });
 
-    startQuiz();  // Initial quiz start
+  
+    function showNextQuestion() {
+      const questionContainers = document.querySelectorAll('.question-container');
+      questionContainers[appstate.currentQuestionIndex].classList.remove('active-question');
+      appstate.currentQuestionIndex += 1;
 
+      if (appstate.currentQuestionIndex < questionContainers.length) {
+        questionContainers[appstate.currentQuestionIndex].classList.add('active-question');
+      } 
+      appstate.qanswered = appstate.qanswered+1;
+    }
+    if(appstate.qanswered===4){
+        alert("Rbgrb");
+    }
+  });
